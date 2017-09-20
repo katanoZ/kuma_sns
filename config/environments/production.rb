9996b09939505cc01e7716bbc7ab74dc869ea340
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-09-20 21:28:03 +0900

    [add]heroku設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://fathomless-spire-59127.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
   #config.web_console.whitelisted_ips = 'crient-server-address'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
